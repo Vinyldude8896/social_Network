aecd207808ee523f389c43bc263afe206bf42810
Kevin Reid <kevinnivek@me.com>
2022-08-29 12:51:57 -0400

    server setup complete

---
+++ server.js
@@ -0,0 +1,27 @@
+// import mongoose and express
+const mongoose = require('mongoose');
+const express = require('express');
+
+// define app and PORT
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// express uses JSON urlencoding
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// app will be using the routes
+app.use(require('./routes'));
+
+// connection for mongoose
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/social_network', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
